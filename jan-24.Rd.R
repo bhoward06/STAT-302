7de30404b7984b9ecbc182bcbad79b8a33f2aa0d
bhoward06 <89262363+bhoward06@users.noreply.github.com>
2022-01-24 11:50:41 -0500

    Add files via upload

--- jan-24.Rd.R
+++ jan-24.Rd.R
@@ -1,3 +1,8 @@
-#Author: Howard , Date: Jan 24 2022, Purpose: National COVID dataset link
+# Author: Howard , Date: Jan 24 2022, Purpose: National COVID dataset link
 
 DATA <- "https://covidtracking.com/data/download/national-history.csv"
+
+# set working directory
+setwd("/Users/brenayhoward/Downloads")
+
+# read the CSV file in R
